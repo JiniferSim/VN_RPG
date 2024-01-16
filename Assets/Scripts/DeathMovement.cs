c9693ac746236a7cdeb9a0d4a944fc8fdb427bdd
JiniferSim <evsaltsina@gmail.com>
2024-01-16 21:04:23 +0100

    Finishet movement

--- Assets/Scripts/DeathMovement.cs
+++ Assets/Scripts/DeathMovement.cs
@@ -8,10 +8,16 @@ public class DeathMovement : MonoBehaviour
     public Rigidbody2D rb;
     Vector2 movement;
 
+    public Animator animator;
+
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()

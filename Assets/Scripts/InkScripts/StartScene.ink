bdb99ca6367cb0a50ccb49655382f76fbc792005
JiniferSim <evsaltsina@gmail.com>
2024-02-23 19:10:52 +0100

    choices are made!

--- Assets/Scripts/InkScripts/StartScene.ink
+++ Assets/Scripts/InkScripts/StartScene.ink
@@ -8,3 +8,8 @@ Darling, I understand, but we've already arranged for Lord Casilim to be there.
 What if the lord were to suddenly disappear?
 {Name("Sevt")}
 ...
+{Name("Death")}
+* That won't work?
+That won't work?
+*[Why don't you say anything?]
+Why don't you say anything?
--- Assets/Scripts/VN dilogue system/ScriptReader.cs
+++ Assets/Scripts/VN dilogue system/ScriptReader.cs
@@ -7,12 +7,14 @@ using TMPro;
 
 public class ScriptReader : MonoBehaviour
 {
-    [SerializeField]
-    private TextAsset _InkJsonFile;
+    [SerializeField] private TextAsset _InkJsonFile;
     private Story _StoryScript;
 
     public TMP_Text dialogueBox;
     public TMP_Text nameTag;
+
+    [SerializeField] private GridLayoutGroup choiceHolder;
+    [SerializeField] private Button choiceBasePrefab;
 
     void Start()
     {
@@ -40,12 +42,57 @@ public class ScriptReader : MonoBehaviour
             text = text?.Trim();
             dialogueBox.text = text;
         }
+        else if(_StoryScript.currentChoices.Count > 0)
+        {
+            DisplayChoices();
+        }
         else
         {
             dialogueBox.text = "The end.";
         }
     }
 
+    private void DisplayChoices()
+    {
+        if (choiceHolder.GetComponentsInChildren<Button>().Length > 0) return; //Checks if button holder has buttons on it
+        for (int i = 0; i < _StoryScript.currentChoices.Count; i++)
+        { 
+            var choice = _StoryScript.currentChoices[i];
+            var button = CreateChoiceButton(choice.text); //creates a choice button
+
+            button.onClick.AddListener(() => OnClickChoiceButton(choice));
+        }
+    }
+
+    Button CreateChoiceButton(string text)
+    {
+        var choiceButton = Instantiate(choiceBasePrefab);
+        choiceButton.transform.SetParent(choiceHolder.transform, false);
+
+        //Change the text on the button prefab
+        var buttonText = choiceButton.GetComponentInChildren<TMP_Text>();
+        buttonText.text = text;
+
+        return choiceButton;
+    }
+
+    void OnClickChoiceButton(Choice choice)
+    {
+        _StoryScript.ChooseChoiceIndex(choice.index);
+        RefreshChoiceView();
+        DisplayNextLine();
+    }
+
+    void RefreshChoiceView()
+    {
+        if(choiceHolder != null)
+        {
+            foreach (var button in choiceHolder.GetComponentsInChildren<Button>())
+            {
+                Destroy(button.gameObject);
+            }
+        }
+    }
     public void ChangeName(string name)
     {
         string SpeakerName = name;
